f57bdc85e9727e1483b2790a64af294fb17b60b9
xilanhuaer <1445106428@qq.com>
2023-10-22 01:23:49 +0800

    增加跨域处理

--- server/main.go
+++ server/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"interface/global"
 	"interface/router"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	global.Connection("./config/config.yaml")
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-Length"},
+		MaxAge:           12 * time.Hour,
+	}))
 	router.Register(r)
 	if global.DB != nil {
 		db, _ := global.DB.DB()
