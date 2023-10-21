8aa8803b87b87c1429747210ce8273864ccd38a0
xilanhuaer <1445106428@qq.com>
2023-10-22 00:46:48 +0800

    fix bug with user

--- server/model/entity/user.go
+++ server/model/entity/user.go
@@ -1,18 +1,24 @@
 package entity
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type User struct {
-	Id        uint   `gorm:"primary_key;auto_increment" json:"id"`
-	Account   string `gorm:"type:varchar(24);not null;unique" json:"account"`
-	Password  string `gorm:"type:varchar(24);not null" json:"password"`
-	Name      string `gorm:"type:varchar(24);not null" json:"name"`
-	Email     string `gorm:"type:varchar(24);not null" json:"email"`
-	Phone     string `gorm:"type:varchar(24);not null" json:"phone"`
-	Enabled   string `gorm:"type:varchar(1);not null;default:1" json:"enabled"`
-	Creator   string `gorm:"type:varchar(24)" json:"creator"`
-	Updator   string `gorm:"type:varchar(24)" json:"updator"`
-	CreatedAt string `gorm:"type:datetime" json:"created_at"`
-	UpdatedAt string `gorm:"type:datetime" json:"updated_at"`
-	DeletedAt string `gorm:"type:datetime" json:"deleted_at"`
+	Id        uint           `gorm:"primary_key;auto_increment" json:"id"`
+	Account   string         `gorm:"type:varchar(24);not null;unique" json:"account"`
+	Password  string         `gorm:"type:varchar(24);not null" json:"password"`
+	Name      string         `gorm:"type:varchar(24);not null" json:"name"`
+	Email     string         `gorm:"type:varchar(24);not null" json:"email"`
+	Phone     string         `gorm:"type:varchar(24);not null" json:"phone"`
+	Enabled   string         `gorm:"type:varchar(1);not null;default:1" json:"enabled"`
+	Creator   string         `gorm:"type:varchar(24)" json:"creator"`
+	Updator   string         `gorm:"type:varchar(24)" json:"updator"`
+	CreatedAt time.Time      `gorm:"type:datetime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"type:datetime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"type:datetime" json:"deleted_at"`
 }
 
 func (User) TableName() string {
