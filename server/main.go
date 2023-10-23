965ab627335e3de51dbbb4620084d859da876555
xilanhuaer <1445106428@qq.com>
2023-10-23 01:19:33 +0000

    alter main.go cors

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"interface/global"
 	"interface/router"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,14 @@ import (
 func main() {
 	global.Connection("./config/config.yaml")
 	r := gin.Default()
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	router.Register(r)
 	if global.DB != nil {
 		db, _ := global.DB.DB()
