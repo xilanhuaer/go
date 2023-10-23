f3aa82efdd309e884855dd9c0c568daa51d32c6f
xilanhuaer <1445106428@qq.com>
2023-10-23 22:34:40 +0800

    再一次修改cors

--- server/middlewares/cors.go
+++ server/middlewares/cors.go
@@ -1,23 +1,17 @@
 package middlewares
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func Cors() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		method := ctx.Request.Method
-		origin := ctx.Request.Header.Get("Origin")
-		if origin != "" {
-			ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-			ctx.Header("Access-Control-Allow-Credentials", "true")
-		}
-		if method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusNoContent)
-		}
-		ctx.Next()
-	}
-}
+// func Cors() gin.HandlerFunc {
+// 	return func(ctx *gin.Context) {
+// 		method := ctx.Request.Method
+// 		origin := ctx.Request.Header.Get("Origin")
+// 		if origin != "" {
+// 			ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+// 			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+// 			ctx.Header("Access-Control-Allow-Credentials", "true")
+// 		}
+// 		if method == "OPTIONS" {
+// 			ctx.AbortWithStatus(http.StatusNoContent)
+// 		}
+// 		ctx.Next()
+// 	}
+// }
