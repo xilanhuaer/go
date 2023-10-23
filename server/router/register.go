cff0bf577794c15e77641b4a02168e6995d114fd
xilanhuaer <1445106428@qq.com>
2023-10-23 23:22:12 +0800

    last

--- server/router/register.go
+++ server/router/register.go
@@ -22,8 +22,8 @@ func Register(route *gin.Engine) {
 	mainCollectionGroup.Use(cors.Default())
 	mainCollectionApi := api.ApiGroupApp.ControllerApiGroup.MainCollectionApi
 	{
-		mainCollectionGroup.POST("/", mainCollectionApi.CreateMainCollection)
-		mainCollectionGroup.GET("/", mainCollectionApi.FindMainCollections)
+		mainCollectionGroup.POST("", mainCollectionApi.CreateMainCollection)
+		mainCollectionGroup.GET("", mainCollectionApi.FindMainCollections)
 		mainCollectionGroup.GET("/:id", mainCollectionApi.FindMainCollection)
 		mainCollectionGroup.PUT("/:id", mainCollectionApi.UpdateMainCollection)
 		mainCollectionGroup.PUT("/enable/:id", mainCollectionApi.CheckMainCollectionEnable)
@@ -34,8 +34,8 @@ func Register(route *gin.Engine) {
 	subCollectionApi := api.ApiGroupApp.ControllerApiGroup.SubCollectionApi
 
 	{
-		subCollectionGroup.POST("/", subCollectionApi.CreateSubCollection)
-		subCollectionGroup.GET("/", subCollectionApi.FindSubCollections)
+		subCollectionGroup.POST("", subCollectionApi.CreateSubCollection)
+		subCollectionGroup.GET("", subCollectionApi.FindSubCollections)
 		subCollectionGroup.GET("/:id", subCollectionApi.FindSubCollection)
 		subCollectionGroup.PUT("/:id", subCollectionApi.UpdateSubCollection)
 		subCollectionGroup.PUT("/enable/:id", subCollectionApi.CheckSubCollectionEnable)
@@ -45,8 +45,8 @@ func Register(route *gin.Engine) {
 	interfaceGroup.Use(cors.Default())
 	interfaceApi := api.ApiGroupApp.ControllerApiGroup.InterfaceApi
 	{
-		interfaceGroup.POST("/", interfaceApi.CreateInterface)
-		interfaceGroup.GET("/", interfaceApi.FindInterfaces)
+		interfaceGroup.POST("", interfaceApi.CreateInterface)
+		interfaceGroup.GET("", interfaceApi.FindInterfaces)
 		interfaceGroup.GET("/:id", interfaceApi.FindInterface)
 		interfaceGroup.PUT("/:id", interfaceApi.UpdateInterface)
 		interfaceGroup.PUT("/enable/:id", interfaceApi.CheckInterfaceEnable)
@@ -56,12 +56,12 @@ func Register(route *gin.Engine) {
 	interfaceImplGroup.Use(cors.Default())
 	interfaceImplApi := api.ApiGroupApp.ControllerApiGroup.InterfaceImplApi
 	{
-		interfaceImplGroup.POST("/", interfaceImplApi.CreateImpl)
+		interfaceImplGroup.POST("", interfaceImplApi.CreateImpl)
 	}
 	requestController := api.ApiGroupApp.ControllerApiGroup.RequestController
 	requestGroup := route.Group("/v1/request")
 	requestGroup.Use(cors.Default())
 	{
-		requestGroup.GET("/", requestController.TestRequest)
+		requestGroup.GET("", requestController.TestRequest)
 	}
 }
