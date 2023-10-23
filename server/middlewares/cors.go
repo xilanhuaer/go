82d25d6e8dbee0d96c26bfbc36ba0e76ef64408a
xilanhuaer <1445106428@qq.com>
2023-10-23 14:15:37 +0000

    fix

--- server/middlewares/cors.go
+++ server/middlewares/cors.go
@@ -11,8 +11,6 @@ func Cors() gin.HandlerFunc {
 		method := ctx.Request.Method
 		origin := ctx.Request.Header.Get("Origin")
 		if origin != "" {
-			ctx.Header("Access-Control-Allow-Origin", "http://111.230.89.67")
-			ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 			ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 			ctx.Header("Access-Control-Allow-Credentials", "true")
