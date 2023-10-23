532923e9cbff2bdc8b06a155ad5b310de47f45bc
xilanhuaer <1445106428@qq.com>
2023-10-23 08:33:06 +0000

    1

--- server/middlewares/cors.go
+++ server/middlewares/cors.go
@@ -1,25 +1,42 @@
 package middlewares
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		method := ctx.Request.Method
-		origin := ctx.Request.Header.Get("Origin")
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin") //请求头部
 		if origin != "" {
-			ctx.Header("Access-Control-Allow-Origin", "http://111.230.89.67")
-			ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-			ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-			ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
-			ctx.Header("Access-Control-Allow-Credentials", "true")
+			//接收客户端发送的origin （重要！）
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			//服务器支持的所有跨域请求的方法
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE,UPDATE")
+			//允许跨域设置可以返回其他子段，可以自定义字段
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session")
+			// 允许浏览器（客户端）可以解析的头部 （重要）
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+			//设置缓存时间
+			c.Header("Access-Control-Max-Age", "172800")
+			//允许客户端传递校验信息比如 cookie (重要)
+			c.Header("Access-Control-Allow-Credentials", "true")
 		}
+
+		//允许类型校验
 		if method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusNoContent)
+			c.JSON(http.StatusOK, "ok!")
 		}
-		ctx.Next()
+
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("Panic info is: %v", err)
+			}
+		}()
+
+		c.Next()
 	}
 }
