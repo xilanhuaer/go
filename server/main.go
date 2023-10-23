3ecb7737a56ce966f4031b91a0cf613f3112ef78
xilanhuaer <1445106428@qq.com>
2023-10-23 22:40:33 +0800

    真的最后一次修改cors

--- server/main.go
+++ server/main.go
@@ -4,12 +4,15 @@ import (
 	"interface/global"
 	"interface/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	global.Connection("./config/config.yaml")
-	r := gin.Default()
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(cors.Default())
 	router.Register(r)
 	if global.DB != nil {
 		db, _ := global.DB.DB()
