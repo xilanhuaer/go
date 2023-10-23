9aad08c56dce94da4ff351d8c17b510213804f5a
xilanhuaer <1445106428@qq.com>
2023-10-23 05:43:17 +0000

    alter main

--- server/main.go
+++ server/main.go
@@ -15,8 +15,6 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
