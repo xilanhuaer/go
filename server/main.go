79b22bbbad65c8058b7e020fb8c54080092398d6
xilanhuaer <1445106428@qq.com>
2023-10-24 01:53:47 +0000

    成功修改cors问题，之前在result中封装了一个cors的返回头，导致中间件不生效

--- server/main.go
+++ server/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"interface/global"
 	"interface/router"
-	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,19 +12,7 @@ func main() {
 	global.Connection("./config/config.yaml")
 	r := gin.New()
 	r.Use(gin.Logger())
-	r.Use(func(ctx *gin.Context) {
-		method := ctx.Request.Method
-		origin := ctx.Request.Header.Get("Origin")
-		ctx.Header("Access-Control-Allow-Origin", origin)
-		ctx.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id")
-		ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
-		ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At")
-		ctx.Header("Access-Control-Allow-Credentials", "true")
-		if method == "OPTIONS" {
-			ctx.AbortWithStatus(http.StatusNoContent)
-		}
-		ctx.Next()
-	})
+	r.Use(cors.Default())
 	router.Register(r)
 	if global.DB != nil {
 		db, _ := global.DB.DB()
