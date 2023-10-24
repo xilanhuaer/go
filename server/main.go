955b42eb5728f9c7bf6d355241e72dcdda9ecd3a
xilanhuaer <1445106428@qq.com>
2023-10-24 02:03:52 +0000

    删除代码处理cors逻辑，改为nginx处理

--- server/main.go
+++ server/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"interface/global"
 	"interface/router"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,13 +11,6 @@ func main() {
 	global.Connection("./config/config.yaml")
 	r := gin.New()
 	r.Use(gin.Logger())
-	r.Use(cors.New(cors.Config{
-		AllowAllOrigins:  true,
-		AllowMethods:     []string{"*"},
-		AllowHeaders:     []string{"*"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 	router.Register(r)
 	if global.DB != nil {
 		db, _ := global.DB.DB()
