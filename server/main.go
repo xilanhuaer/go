74c7479f6d69a6413ad80bf2749747df09568239
xilanhuaer <1445106428@qq.com>
2023-10-24 00:30:27 +0000

    修改cors中间件

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"interface/global"
 	"interface/router"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -12,7 +13,13 @@ func main() {
 	global.Connection("./config/config.yaml")
 	r := gin.New()
 	r.Use(gin.Logger())
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	router.Register(r)
 	if global.DB != nil {
 		db, _ := global.DB.DB()
