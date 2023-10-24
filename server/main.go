1d16ef8938637da19e17fa8e4d25542d5220e157
xilanhuaer <1445106428@qq.com>
2023-10-24 01:47:38 +0000

    修改cors

--- server/main.go
+++ server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"interface/global"
 	"interface/router"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,8 +15,7 @@ func main() {
 	r.Use(func(ctx *gin.Context) {
 		method := ctx.Request.Method
 		origin := ctx.Request.Header.Get("Origin")
-		log.Println(origin)
-		ctx.Header("Access-Control-Allow-Origin", "https://www.baidu.com/")
+		ctx.Header("Access-Control-Allow-Origin", origin)
 		ctx.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token,X-Token,X-User-Id")
 		ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELETE,PUT")
 		ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, New-Token, New-Expires-At")
