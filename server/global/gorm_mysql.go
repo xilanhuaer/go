f07670083cd44dcf3257b03bf0dab86ef24951dd
xilanhuaer <1445106428@qq.com>
2023-10-29 21:28:43 +0800

    初始化gorm时增加缓存预编译，禁用默认事务

--- server/global/gorm_mysql.go
+++ server/global/gorm_mysql.go
@@ -19,7 +19,9 @@ func Connection(path string) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.DatabaseName)
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger:                 logger.Default.LogMode(logger.Info),
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
 	})
 	if err != nil {
 		log.Fatalf("error:%v", err)
