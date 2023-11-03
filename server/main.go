d71ce302875fe3f6c02faa9539c0918a112fc9e7
xilanhuaer <1445106428@qq.com>
2023-11-03 03:31:12 +0000

    增加cors

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"interface/middleware"
 	"interface/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,7 @@ func main() {
 	global.Connection("./config/config.yaml")
 	r := gin.New()
 	r.Use(gin.Logger())
+	r.Use(cors.Default())
 	jwt := &middleware.JWTAuthMiddleware{}
 	r.Use(jwt.JWTAuthMiddleware())
 	router.Register(r)
